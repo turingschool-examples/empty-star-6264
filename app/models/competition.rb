48c79781360266989e5b4b4eca6bd657e388b6d8
Blake Dempsey <bdempsey864@gmail.com>
2021-09-28 10:23:16 -0600

    Finish user story 2

--- app/models/competition.rb
+++ app/models/competition.rb
@@ -1,4 +1,9 @@
 class Competition < ApplicationRecord
   has_many :team_comps
   has_many :teams, through: :team_comps
+  has_many :players, through: :teams
+
+  # def average_age
+  #   require "pry"; binding.pry
+  # end
 end
